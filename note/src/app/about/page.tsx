86e130879cbb58b37aa9c0ab3617ff2c17486411
kjy <spacejay1007@gmail.com>
2023-04-20 18:57:53 +0900

    next

---
+++ note/src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const index = (): JSX.Element => {
+  return <div>???</div>;
+};
+
+export default index;
--- note/src/app/pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const About: React.FC = (): JSX.Element => {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-};
