a06de8749ff4961c84128a899764ac428384a2be
kjy <spacejay1007@gmail.com>
2023-05-04 19:01:02 +0900

    next JS

--- my-blog/src/app/posts/[slug]/page.tsx
+++ my-blog/src/app/posts/[slug]/page.tsx
@@ -1,11 +1,23 @@
+import { getPost } from "@/service/post";
+
 type Props = {
   params: {
     slug: string;
   };
 };
 
-const postSlugPage = ({ params }: Props) => {
-  return <div>각 postPage</div>;
+const postSlugPage = async ({ params }: Props) => {
+  console.log(params);
+  const post = await getPost(params.slug);
+  console.log(post);
+
+  return (
+    <div>
+      <div>{post?.image}</div>
+      <div>{post?.title}</div>
+      <div>{post?.desc}</div>
+    </div>
+  );
 };
 
 export default postSlugPage;
