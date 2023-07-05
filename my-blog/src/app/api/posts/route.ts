c1f504aa823b98e1282b73eb03461eb81727902d
Jay <spacejay1007@gmail.com>
2023-07-06 00:02:56 +0900

    mongoDB GET Connect

--- my-blog/src/app/api/posts/route.ts
+++ my-blog/src/app/api/posts/route.ts
@@ -4,45 +4,48 @@ import path from "path";
 import fs from "fs";
 import { getAllPosts } from "service/post";
 // import Post from "lib/schemas/posts";
-import { MongoClient } from "mongodb";
+import { DbOptions, MongoClient } from "mongodb";
 import { QueryError, createConnection } from "mysql2";
 import { NextApiRequest, NextApiResponse } from "next";
 import { posts } from "lib/schemas/posts";
 
 export const GET = async (req: Request, res: Response) => {
-  // const testPost = Test;
-  // const allTests = await posts.findAll({});
-  // await connectMongoDB();
-  // await connect();
+  // My SQL
+  // const sqlConnect = await createConnection({
+  //   host: "localhost",
+  //   // host: process.env.MYSQL_HOST,
+  //   // port: process.env.MYSQL_PORT,
+  //   user: process.env.SQL_USER,
+  //   password: process.env.SQL_PW,
+  //   database: "nodejs",
+  // });
+  // // const queryString =
 
-  // const db = mongoConnect.db();
-  //
-  const sqlConnect = await createConnection({
-    host: "localhost",
-    // host: process.env.MYSQL_HOST,
-    // port: process.env.MYSQL_PORT,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PW,
-    database: "nodejs",
-  });
-  // const queryString =
+  // sqlConnect.query(
+  //   "SELECT * FROM nodejs.posts",
+  //   (err: QueryError, result: any) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log(result);
+  //       // NextResponse.json(result);
+  //       sqlConnect.end();
+  //       return NextResponse.json(result);
+  //     }
+  //   }
+  // );
 
-  sqlConnect.query(
-    "SELECT * FROM nodejs.posts",
-    (err: QueryError, result: any) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-        // NextResponse.json(result);
-        sqlConnect.end();
-        return NextResponse.json(result);
-      }
-    }
-  );
+  // MongoDB
+  // mongoDB GET
+  const client = await MongoClient.connect(process.env.MONGODB_URI || "");
+  const db = client.db("posts");
+  const postsCollection = db.collection("posts");
+  const result = await postsCollection.find({}).toArray();
+
+  client.close();
   // const posts = await getAllPosts();
   // console.log(postsss);
-  return NextResponse.json("");
+  return NextResponse.json(result);
 };
 
 export const POST = async (req: Request, res: Response) => {
@@ -50,12 +53,9 @@ export const POST = async (req: Request, res: Response) => {
   // const client = await MongoClient.connect(process.env.MONGODB_URI || "");
   if (!postData) return NextResponse.json({ message: "Missing Data" });
 
-  // const posts = Post;
-  // console.log(posts);
-
   // mongoDB POST
   const client = await MongoClient.connect(process.env.MONGODB_URI || "");
-  const db = client.db();
+  const db = client.db("posts");
 
   const postsCollection = db.collection("posts");
   const result = await postsCollection.insertOne(postData);
@@ -83,7 +83,7 @@ export const POST = async (req: Request, res: Response) => {
 
   // const sqlDb = sqlConnect.query
 
-  //
+  // JSON DATA 받아오기
   // const postData = await req.json(); // 보내준 JSON 데이터를 받아 데이터를 담아준다.
   // // const testId = await getNextSequenceValue("testId");
 
